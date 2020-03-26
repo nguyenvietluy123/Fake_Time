bcd7df7a44d63d11b591174c56038ac86437d39d
nguyen.viet.luy <nguyen.viet.luy@sun-asterisk.com>
2020-03-26 18:12:32 +0700

    edit pod

--- FakeTime/AppDelegate.swift
+++ FakeTime/AppDelegate.swift
@@ -14,7 +14,7 @@ let kAdmobAppID         = "ca-app-pub-1947012962477196~8459063867"
 let kAdmobBanner        = "ca-app-pub-1947012962477196/2412530261"
 let kAdmobInterstitial  = "ca-app-pub-1947012962477196/2272929463"
 
-
+12
 //let kAdmobAppID         = ""
 //let kAdmobBanner        = ""
 //let kAdmobInterstitial  = ""
